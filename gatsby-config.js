56052762c33e3e6bfa2dad82286bee30b81e588c
chrisrzhou <chrisrzhou@pm.me>
2021-06-05 22:10:36 -0700

    docs: add ui-systems-and-complexity-whitepaper.mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,14 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-mdx',
       options: {
+        defaultLayouts: {
+          default: require.resolve('./src/layouts/page-layout.js'),
+        },
         extensions: ['.md', '.mdx'],
+        gatsbyRemarkPlugins: [
+          'gatsby-remark-table-of-contents',
+          'gatsby-remark-autolink-headers',
+        ],
       },
     },
   ],
