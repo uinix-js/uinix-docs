b2cd8c30d4f188fd9afa5d8b2ea17eb8c0e2b989
chrisrzhou <chrisrzhou@pm.me>
2021-06-09 00:11:54 -0700

    build: add gatsby-source-github-api, dotenv, remark-highlight.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 module.exports = {
   siteMetadata: {
     title: 'uinix-docs',
@@ -18,6 +20,54 @@ module.exports = {
       options: {
         name: 'documents',
         path: 'src/documents',
+      },
+    },
+    {
+      resolve: 'gatsby-source-github-api',
+      options: {
+        token: process.env.GITHUB_API_TOKEN,
+        variables: {
+          query: 'user:uinix-js is:public archived:false sort:stars',
+        },
+        graphQLQuery: `
+          query github($query: String!) {
+            repos: search(query: $query, type: REPOSITORY, first: 100) {
+              edges {
+                node {
+                  ... on Repository {
+                    name
+                    description
+                    homepageUrl
+                    url
+                    pushedAt
+                    repositoryTopics(first: 100) {
+                      edges {
+                        node {
+                          topic {
+                            name
+                          }
+                        }
+                      }
+                    }
+                    stargazers {
+                      totalCount
+                    }
+                    readme: object(expression: "main:readme.md") {
+                      ... on Blob {
+                        text
+                      }
+                    }
+                    package: object(expression: "main:package.json") {
+                      ... on Blob {
+                        text
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        `,
       },
     },
   ],
