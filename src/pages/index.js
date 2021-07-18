d831b7b97aada13ad307c8d98a2193579dd47ec0
chrisrzhou <chrisrzhou@pm.me>
2021-07-18 03:31:41 -0700

    update homepage

--- src/pages/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import PageLayout from '../layouts/page-layout.js';
-
-const Page = () => (
-  <PageLayout>🏗 Site under construction. Check back soon!</PageLayout>
-);
-
-export default Page;
